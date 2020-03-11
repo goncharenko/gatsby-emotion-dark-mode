8efa7fc44706673a86a8b7a6aed41f140b62d743
Vitaly Goncharenko <vitaliy.goncharenko@hotmail.com>
2020-03-12 00:57:37 +0800

    add linter config, prettier config, fix gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        browser: true,
+        node: true
+    },
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off'
+    },
+};
